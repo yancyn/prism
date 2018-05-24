1783b56981bc1004dece9f31e1dc24efc32033f5
yancyn <yancyn@gmail.com>
2018-05-24 19:47:15 +0800

    Add new module project

---
+++ TouchstoneModule/TouchstoneModule.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace TouchstoneModule
+{
+    public class TouchstoneModule : IModule
+    {
+        private readonly IRegionViewRegistry regionViewRegistry;
+        public TouchstoneModule(IRegionViewRegistry registry)
+        {
+            this.regionViewRegistry = registry;
+        }
+        public void Initialize()
+        {
+            regionViewRegistry.RegisterViewWithRegion("ToolbarRegion", typeof(TouchstoneView));
+        }
+    }
+}
